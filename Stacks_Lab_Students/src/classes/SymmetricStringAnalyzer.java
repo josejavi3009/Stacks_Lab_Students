477842996494dc806c07ddeeb3e4b4e65e89722a
Jose Vazquez <31424499+josejavi3009@users.noreply.github.com>
2019-10-20 21:34:45 -0400

    Update SymmetricStringAnalyzer.java

--- Stacks_Lab_Students/src/classes/SymmetricStringAnalyzer.java
+++ Stacks_Lab_Students/src/classes/SymmetricStringAnalyzer.java
@@ -1,31 +1,54 @@
 package classes;
-
 public class SymmetricStringAnalyzer {
 	private String s; 
 	public SymmetricStringAnalyzer(String s) {
 		this.s = s; 
 	}
-	
+
 	/**
 	 * Determines if the string s is symmetric
 	 * @return true if it is; false, otherwise. 
 	 */
 	public boolean isValidContent() { 
-		// ADD MISSING CODE
-		
-		return true;  // need to change if necessary....
+		LLStack<Character> stack = new LLStack<>(); 
+		for (int i=0; i < s.length(); i++) { 
+			char c = s.charAt(i); 
+			if (Character.isLetter(c))
+				if (Character.isUpperCase(c))
+					stack.push(c);
+				else if (stack.isEmpty())
+					return false;
+				else {
+					char t = stack.top(); 
+					if (t == Character.toUpperCase(c))
+						stack.pop();  
+					else 
+						return false; 
+				}
+			else 
+				return false; 
+		} 
+		return stack.isEmpty();
 	}
-	
+
 	public String toString() { 
 		return s; 
 	}
 
 	public String parenthesizedExpression() 
-	throws StringIsNotSymmetricException 
-	{
-		// ADD MISSING CODE
-		
-		return null;  // need to change if necessary....
+			throws StringIsNotSymmetricException {
+		String str = "";
+		if(this.isValidContent()){
+			for(int i = 0; i < s.length(); i++){
+				if(Character.isUpperCase(s.charAt(i))){
+					str += " <" + s.charAt(i) + " ";
+				}
+				else{
+					str += " " + s.charAt(i) + ">" + " ";
+				}
+			}
+		}
+		return str;
 	}
 
 }
